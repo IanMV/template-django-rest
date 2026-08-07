dcbc0eea1c2813b9900850dd10b1ea7d2d173664
IanMV <ianmatheusv54@gmail.com>
2026-08-06 22:06:07 -0300

    feat: model customizada de usuário, seu painel de admin e suas migrações

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,99 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminFromDjango
+from core.models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(UserAdminFromDjango):
+    ordering = None
+
+    list_display = (
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "email",
+        "groups__name",
+        "user_permissions__name",
+        "user_permissions__codename",
+    )
+
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                    "id",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "created_at",
+                )
+            },
+        ),
+        (
+            "Groups",
+            {"fields": ("groups",)},
+        ),
+        (
+            "User Permissions",
+            {"fields": ("user_permissions",)},
+        ),
+    )
+
+    readonly_fields = (
+        "last_login",
+        "id",
+        "created_at",
+    )
+
+    add_fieldsets = (
+        (
+            "Required",
+            {
+                "classes": [
+                    "collapse",
+                ],
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            "Optional",
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
--- core/models.py
+++ core/models.py
@@ -1,3 +1,85 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+import uuid
+from django.utils.timezone import now
 from django.db import models
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(
+                "The email address is required.",
+            )
+
+        email = self.normalize_email(
+            email,
+        )
+
+        user = self.model(
+            email=email,
+            **extra_fields,
+        )
+
+        user.set_password(
+            password,
+        )
+
+        user.save(
+            using=self._db,
+        )
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault(
+            "is_staff",
+            True,
+        )
+
+        extra_fields.setdefault(
+            "is_superuser",
+            True,
+        )
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields,
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=now(),
+    )
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
