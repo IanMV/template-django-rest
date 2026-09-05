a99b949b3f23ed09e6479b9ec289a1a9bd16ce40
IanMV <ianmatheusv54@gmail.com>
2026-09-05 10:53:11 -0300

    feat: adds tables for codes sent via email

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,1 +1,5 @@
 from core.models.custom_user import User as User
+from core.models.email_change import EmailChange as EmailChange
+from core.models.email_verification import EmailVerification as EmailVerification
+from core.models.one_time_code import OneTimeCode as OneTimeCode
+from core.models.password_reset import PasswordReset as PasswordReset
