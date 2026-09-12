e10eb16d04a379d6d038a6e6fc5a9f7bb6511f62
IanMV <ianmatheusv54@gmail.com>
2026-09-12 19:57:11 -0300

    fix: serializers imports in __init__.py

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,9 +1,36 @@
 from core.serializers.custom_user import (
-    UserCreateSerializer as UserCreateSerializer,
+    EmailChangeConfirmSerializer as EmailChangeConfirmSerializer,
+)
+from core.serializers.custom_user import (
+    EmailChangeRequestSerializer as EmailChangeRequestSerializer,
+)
+from core.serializers.custom_user import (
+    EmailChangeVerifySerializer as EmailChangeVerifySerializer,
+)
+from core.serializers.custom_user import (
+    LoginSerializer as LoginSerializer,
+)
+from core.serializers.custom_user import (
+    LogoutSerializer as LogoutSerializer,
+)
+from core.serializers.custom_user import (
+    PasswordResetConfirmSerializer as PasswordResetConfirmSerializer,
+)
+from core.serializers.custom_user import (
+    PasswordResetRequestSerializer as PasswordResetRequestSerializer,
+)
+from core.serializers.custom_user import (
+    PasswordResetVerifySerializer as PasswordResetVerifySerializer,
+)
+from core.serializers.custom_user import (
+    RegistrationConfirmSerializer as RegistrationConfirmSerializer,
+)
+from core.serializers.custom_user import (
+    RegistrationRequestSerializer as RegistrationRequestSerializer,
+)
+from core.serializers.custom_user import (
+    RegistrationVerifySerializer as RegistrationVerifySerializer,
 )
 from core.serializers.custom_user import (
     UserListRetrieveSerializer as UserListRetrieveSerializer,
-)
-from core.serializers.custom_user import (
-    UserPatchSerializer as UserPatchSerializer,
 )
